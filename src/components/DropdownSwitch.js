bd60e768d0a630d9319eb7766ae62e7a2da18f39
vynvan_server <giant_innovations@live.de>
2024-11-14 12:37:40 +0100

    css correction for nav dropdown menu

--- src/components/DropdownSwitch.js
+++ src/components/DropdownSwitch.js
@@ -1,13 +1,11 @@
 import { Form } from 'react-bootstrap';
 
-function DropdownSwitch({ labelName }) {
+export default function DropdownSwitch({ labelName }) {
 
    return (
-      <Form.Group className='d-flex dropdown-item'>
+      <Form.Group className='d-flex dropdown-item complex-item'>
          <Form.Label className='mb-0 me-2'>{labelName}</Form.Label>
          <Form.Check name="theme" type="switch" variant="none"></Form.Check>
       </Form.Group>
    );
 }
-
-export default DropdownSwitch;
