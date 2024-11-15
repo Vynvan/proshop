fb33951251fe7836cbf897ca5141a0214aabeb2a
vynvan_server <giant_innovations@live.de>
2024-11-15 14:15:56 +0100

    Writing ArticleDetails as Overlay

--- src/components/Article.js
+++ src/components/Article.js
@@ -1,11 +1,20 @@
-import { Card, Container } from 'react-bootstrap';
+import { Button, Card } from 'react-bootstrap';
 
-export default function Article({ data }) {
+export default function Article({ data, onClick }) {
    return (
-      <Card className='product p-lg-4 p-md-3 p-2'>
-         <Card.Img className='p-1' src="2922280_27002.jpg" />
-         <Card.Title>{data.title}</Card.Title>
-         <Card.Text>{data.text}</Card.Text>
+      <Card className='product'>
+         <Card.Img className='mb-1' src={data.image} srcSet='2922280_27002.jpg' onClick={onClick} />
+         <Card.Title className='px-lg-4 px-md-3 px-2' onClick={onClick}>{data.title}</Card.Title>
+         <Card.Text className='px-lg-4 px-md-3 px-2'>{data.text}</Card.Text>
+         <Card.Footer className='d-flex align-items-center justify-content-between'>
+            <span>{data.price} €</span>
+            <Button className='rounded-circle'>
+               <svg xmlns="http://www.w3.org/2000/svg" fill="currentColor" className="bi bi-cart-plus" viewBox="0 0 16 16">
+                  <path d="M9 5.5a.5.5 0 0 0-1 0V7H6.5a.5.5 0 0 0 0 1H8v1.5a.5.5 0 0 0 1 0V8h1.5a.5.5 0 0 0 0-1H9z"/>
+                  <path d="M.5 1a.5.5 0 0 0 0 1h1.11l.401 1.607 1.498 7.985A.5.5 0 0 0 4 12h1a2 2 0 1 0 0 4 2 2 0 0 0 0-4h7a2 2 0 1 0 0 4 2 2 0 0 0 0-4h1a.5.5 0 0 0 .491-.408l1.5-8A.5.5 0 0 0 14.5 3H2.89l-.405-1.621A.5.5 0 0 0 2 1zm3.915 10L3.102 4h10.796l-1.313 7zM6 14a1 1 0 1 1-2 0 1 1 0 0 1 2 0m7 0a1 1 0 1 1-2 0 1 1 0 0 1 2 0"/>
+               </svg>
+            </Button>
+         </Card.Footer>
       </Card>
    );
 }
--- src/pages/Articles.js
+++ src/pages/Articles.js
@@ -1,54 +1,81 @@
-import { Col, Row } from "react-bootstrap";
-import Article from "../components/Article";
+import { useRef, useState } from 'react';
+import { Col, Row } from 'react-bootstrap';
+import Article from '../components/Article';
+import ArticleDetails from '../components/ArticleDetails';
 
 export default function Articles() {
    const testArticles = [
       {
-          title: "Artikel 1",
-          text: "Dies ist der Text für Artikel 1.",
-          image: "https://example.com/image1.jpg",
-          price: 19.99,
-          product_id: "prod_001"
+         title: 'Artikel 1',
+         text: 'Dies ist der Text für Artikel 1.',
+         image: 'https://example.com/image1.jpg',
+         price: 19.99,
+         product_id: 'prod_001',
       },
       {
-          title: "Artikel 2",
-          text: "Dies ist der Text für Artikel 2.",
-          image: "https://example.com/image2.jpg",
-          price: 29.99,
-          product_id: "prod_002"
+         title: 'Artikel 2',
+         text: 'Dies ist der Text für Artikel 2.',
+         image: 'https://example.com/image2.jpg',
+         price: 29.99,
+         product_id: 'prod_002',
       },
       {
-          title: "Artikel 3",
-          text: "Dies ist der Text für Artikel 3.",
-          image: "https://example.com/image3.jpg",
-          price: 39.99,
-          product_id: "prod_003"
+         title: 'Artikel 3',
+         text: 'Dies ist der Text für Artikel 3.',
+         image: 'https://example.com/image3.jpg',
+         price: 39.99,
+         product_id: 'prod_003',
       },
       {
-          title: "Artikel 4",
-          text: "Dies ist der Text für Artikel 4.",
-          image: "https://example.com/image4.jpg",
-          price: 49.99,
-          product_id: "prod_004"
+         title: 'Artikel 4',
+         text: 'Dies ist der Text für Artikel 4.',
+         image: 'https://example.com/image4.jpg',
+         price: 49.99,
+         product_id: 'prod_004',
       },
       {
-          title: "Artikel 5",
-          text: "Dies ist der Text für Artikel 5.",
-          image: "https://example.com/image5.jpg",
-          price: 59.99,
-          product_id: "prod_005"
-      }
+         title: 'Artikel 5',
+         text: 'Dies ist der Text für Artikel 5.',
+         image: 'https://example.com/image5.jpg',
+         price: 59.99,
+         product_id: 'prod_005',
+      },
    ];
+   const [selectedArticle, setSelectedArticle] = useState(null);
+   const [showOverlay, setShowOverlay] = useState(false);
+   const overlayTarget = useRef(null);
+
+   function handleClick(article) {
+      setSelectedArticle(article);
+      setShowOverlay(true);
+   }
+
+   function handleCloseOverlay() {
+      setShowOverlay(false);
+      setSelectedArticle(null);
+   }
 
    return (
-    <Row xs={1} sm={2} md={3} lg={4} xl={5} xxl={6} className="mx-1 p-md-3 p-2">
-        {testArticles.map(article => {
-            return (
-                <Col key={article.product_id} className="g-md-3 g-2">
-                    <Article data={article} />
-                </Col>
-            );
-        })}
-    </Row>      
+      <>
+      <div ref={overlayTarget}>
+         <Row xs={1} sm={2} md={3} lg={4} xl={5} xxl={6} className="mx-1 p-md-3 p-2">
+            {testArticles.map((article) => {
+               return (
+                  <Col key={article.product_id} className="g-md-3 g-2">
+                     <Article data={article} onClick={() => handleClick(article)} />
+                  </Col>
+               );
+            })}
+         </Row>
+      </div>
+      {overlayTarget && selectedArticle && (
+         <ArticleDetails
+         show={showOverlay}
+         onHide={handleCloseOverlay}
+         article={selectedArticle}
+         target={overlayTarget.current}
+         />
+      )}
+      </>
    );
 }
