5b71f08ea4021f1fe4a2d31d3b542830d617b8f3
vynvan_server <giant_innovations@live.de>
2024-11-20 14:55:31 +0100

    Moved logout functionality from LogoutToggleButton to UserProvider

--- src/components/LogoutToggleButton.js
+++ src/components/LogoutToggleButton.js
@@ -2,17 +2,12 @@ import { Button, DropdownItem } from 'react-bootstrap';
 import useUser from '../hooks/UserProvider';
 
 function LogoutToggleButton({ asDropdownItem, loginText, onLogin, setMessage, setMessageType, setNotification }) {
-   const { user, setUser } = useUser();
+   const { logout, user } = useUser();
 
    if (!onLogin) onLogin = () => {};
-   const handleLogout = async () => {
-      fetch(`${process.env.REACT_APP_APIURL}/users/logout`, {
-         headers: {  'Content-Type': 'application/json' },
-         method: 'POST',
-         body: JSON.stringify({ userId: user.userId }),
-      });
-   
-      setUser(null);
+
+   const handleLogout = () => {
+      logout();      
       if (setMessage) setMessage('Sie haben sich ausgeloggt. Bis zum nächsten Mal!');
       if (setMessageType) setMessageType('success');
       if (setNotification) setNotification({
@@ -23,7 +18,7 @@ function LogoutToggleButton({ asDropdownItem, loginText, onLogin, setMessage, se
    
    return (
       asDropdownItem ? (
-         <DropdownItem onClick={handleLogout}>Abmelden</DropdownItem>
+         <DropdownItem onClick={() => handleLogout()}>Abmelden</DropdownItem>
       ) : (
       <Button onClick={() => user ? handleLogout() : onLogin()}>
          {user ? 'Abmelden' : loginText ?? 'Anmelden'}
--- src/hooks/UserProvider.js
+++ src/hooks/UserProvider.js
@@ -10,7 +10,17 @@ export function UserProvider({ children }) {
       else localStorage.removeItem('user');
    }, [user]);
 
-   return <UserContext.Provider value={{ user, setUser }}>{children}</UserContext.Provider>;
+   const logout = () => {
+      fetch(`${process.env.REACT_APP_APIURL}/users/logout`, {
+         headers: {  'Content-Type': 'application/json' },
+         method: 'POST',
+         body: JSON.stringify({ userId: user.userId }),
+      });
+   
+      setUser(null);
+   }
+
+   return <UserContext.Provider value={{ logout, user, setUser }}>{children}</UserContext.Provider>;
 };
 
 export default function useUser () {
