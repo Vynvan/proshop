87a16d8dd6935c9ce12b58910e3b8cd7f29e6c7b
vynvan_server <giant_innovations@live.de>
2024-11-22 11:38:43 +0100

    Reduced fetches by putting method out of the effect deps

--- src/hooks/useFetch.js
+++ src/hooks/useFetch.js
@@ -47,7 +47,7 @@ export default function useFetch() {
          }
       }
       doFetch();
-   }, [body, method, navigate, url, user]);
+   }, [body, navigate, url, user]);
 
    const fetchUrl = (url, method='GET', body=undefined) => {
       console.log('Start fetching:', url, method, body)
